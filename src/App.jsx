873444ec5f4b968bf9e88fec48e7ad8051b8728b
Luffy4king <rohitmeruga@gmail.com>
2026-01-22 11:44:42 +0530

    Added app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
  <div className="min-h-screen w-full bg-white overflow-x-hidden">
-
+{/* routing */}
       <Navbar />
       <Dashboard />
     </div>
